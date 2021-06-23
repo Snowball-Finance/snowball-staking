580e2f2b422f6b81512efdb5171ecebb35afa721
bigwampa <big.wampa@protonmail.com>
2021-06-23 14:40:46 -0700

    fix total and distribution date

--- containers/Home/SnowClaim/index.js
+++ containers/Home/SnowClaim/index.js
@@ -23,10 +23,10 @@ const SnowClaim = () => {
       <Grid container spacing={2}>
         <Grid item xs={12}>
           <Typography color='textSecondary' variant='body1'>
-            {`Current total: 45k SNOB`}
+            {`Current total: 20k SNOB`}
           </Typography>
           <Typography color='textSecondary' variant='body1'>
-            {`Current distribution: Wed Jun 02 2021`}
+            {`Current distribution: Wed Jun 16 2021`}
           </Typography>
           <br/>
           <Typography color='textSecondary' variant='body1'>
